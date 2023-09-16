353e582d9161c843b14f3d4f76122438def06d31
Dipen Chauhan <get.dipen@gmail.com>
2023-09-16 00:07:45 -0700

    iterating categories array

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
+import "./categories.styles.scss";
+
 const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: "Hats",
+    },
+    {
+      id: 1,
+      title: "Jackets",
+    },
+    {
+      id: 1,
+      title: "Sneakers",
+    },
+    {
+      id: 1,
+      title: "Womens",
+    },
+    {
+      id: 1,
+      title: "Mens",
+    },
+  ];
+
   return (
     <div className="categories-container">
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Hats</h2>
-          <p>Shop Now</p>
+      {categories.map(({ id, title }) => (
+        <div key={id} className="category-container">
+          {/* <div.background-image /> */}
+          <div className="category-body-container">
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
         </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Jackets</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Sneakers</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Womens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      <div className="category-container">
-        {/* <img /> */}
-        <div className="category-body-container">
-          <h2>Mens</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
